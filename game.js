0fc3a0b32168f637f04876b57de76653b70174f1
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-12-15 16:15:46 +0000

    working maze render

--- game.js
+++ game.js
@@ -1,42 +1,37 @@
 var Game = {
-    display: null,
- 
+    map: {},
+    
     init: function() {
-        this.display = new ROT.Display();
+        this.display = new ROT.Display({spacing:1.1});
         document.body.appendChild(this.display.getContainer());
+        
+        this._generateMap();
+        
+    },
+    
+    _generateMap: function() {
+        var digger = new ROT.Map.Digger();
+        var freeCells = [];
+        
+        var digCallback = function(x, y, value) {
+            if (value) { return; }
+            
+            var key = x+","+y;
+            this.map[key] = ".";
+            freeCells.push(key);
+        }
+        digger.create(digCallback.bind(this));
+        
+        this._drawWholeMap();
+    },
+    
+    _drawWholeMap: function() {
+        for (var key in this.map) {
+            var parts = key.split(",");
+            var x = parseInt(parts[0]);
+            var y = parseInt(parts[1]);
+            this.display.draw(x, y, this.map[key]);
+        }
     }
 }
-
-Game.map = {};
-Game._generateMap = function() {
-    var digger = new ROT.Map.Digger();
-    var freeCells = [];
- 
-    var digCallback = function(x, y, value) {
-        if (value) { return; } /* do not store walls */
- 
-        var key = x+","+y;
-        freeCells.push(key);
-        this.map[key] = ".";
-    }
-    digger.create(digCallback.bind(this));
- 
-    this._generateExit(freeCells);
- 
-    this._drawWholeMap();
-};
- 
-Game._generateExit = function(freeCells) {
-	var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-	var key = freeCells.splice(index, 1)[0];
-	this.map[key] = ">";
-};
-
-Game._drawWholeMap = function() {
-    for (var key in this.map) {
-        var parts = key.split(",");
-        var x = parseInt(parts[0]);
-        var y = parseInt(parts[1]);
-        this.display.draw(x, y, this.map[key]);
-    }
-}
+Game.init();
